473361aa5c7d8a4da5454979428e001f93b70cde
kevfo <75442525+kevfo@users.noreply.github.com>
2022-10-13 01:18:03 +0800

    Finished grid generation and clicking code

--- assets/overall.js
+++ assets/overall.js
@@ -11,5 +11,49 @@ function navigateBetween(tabID, element) {
   }
 
   document.getElementById(tabID).style.display = 'block';
-  element.style.background = '#6683C6';
+  // element.style.background = '#6683C6'; - color bug here
+}
+
+// == FOR LOGIC GRIDS ==
+
+// Makes the grid that we want:
+function makeLogicGrid(nrow, ncol, decrementBy, tableID) {
+  let table = document.getElementById(tableID), rowCounter = 0, tableContents = '';
+  let id = 0;
+
+  for (let y = nrow ; y > 0 ; y--) {
+    let rowBegin = '<tr>', rowEnd = '</tr>';
+
+    for (let x = 0 ; x < ncol ; x++) {
+      rowBegin += `<td class = "logicCell" id = "${x} ${y}" onclick = "clickCell('${x} ${y}')">  </td>`;
+    }
+
+    rowBegin += ` ${rowEnd}`;
+
+    rowCounter++;
+    if (rowCounter === decrementBy) {
+      ncol -= decrementBy;
+      rowCounter = 0;
+    }
+    tableContents += rowBegin.concat(' ', rowEnd);
+  }
+  table.innerHTML += tableContents;
+}
+
+// Handles mouse clicking for logic grid:
+function clickCell(cellID) {
+  let selectedCell = document.getElementById(cellID);
+
+  switch(selectedCell.className) {
+    case "logicCell":
+      selectedCell.className = 'selectedCell';
+      break;
+    case "selectedCell":
+      selectedCell.className = 'cancelCell';
+      break;
+    case "cancelCell":
+      selectedCell.className = 'logicCell';
+      break;
+  }
+  return selectedCell;
 }
